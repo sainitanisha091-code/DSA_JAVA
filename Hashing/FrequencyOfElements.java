be666b09efa4c6c26eeba5331a5eb2e11f3d3612
Tanisha <sainitanisha091@gmail.com>
2026-09-14 10:27:27 +0530

    Add element frequency counting using HashMap
    Finds the frequency of each element in an integer array using HashMap.

---
+++ Hashing/FrequencyOfElements.java
@@ -0,0 +1,37 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the size of array: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter array elements:");
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < arr.length; i++) {
+
+            if (map.containsKey(arr[i])) {
+                map.put(arr[i], map.get(arr[i]) + 1);
+            } else {
+                map.put(arr[i], 1);
+            }
+        }
+
+        System.out.println("Frequency of elements:");
+        System.out.println(map);
+
+        sc.close();
+    }
+}
